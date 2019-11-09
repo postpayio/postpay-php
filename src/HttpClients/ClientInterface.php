56cddc960e94f05ec42fd355ba1c49b241b67664
mongkok <domake.io@gmail.com>
2019-11-09 23:10:50 +0400

    Fixed typos

--- src/HttpClients/ClientInterface.php
+++ src/HttpClients/ClientInterface.php
@@ -10,7 +10,7 @@ interface ClientInterface
      * Sends a request to the server and returns the response.
      *
      * @param Request  $request Request to send.
-     * @param int|null $timout The timeout for the request.
+     * @param int|null $timeout The timeout for the request.
      *
      * @return \Postpay\Http\Response Response from the server.
      *
